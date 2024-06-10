6fe05ac89c46d7948f27bc35ce6b93cd57096198
Christopher Renaud Oelerich <chris@oeleri.ch>
2024-06-09 21:13:34 -0500

    weather task is stopping?

--- weather.py
+++ weather.py
@@ -34,11 +34,11 @@ class Weather(MenuFrame):
             datetime.now().strftime('%I:%M%p').lstrip('0').ljust(7),
             'Weather Loading', Align.LEFT, Align.RIGHT
         )
-        logging.info('weather a')
+        logging.warning('weather a')
         asyncio.create_task(self.update_all())
-        logging.info('weather b')
+        logging.warning('weather b')
         super().__init__(menu, msg)
-        logging.info('weather c')
+        logging.warning('weather c')
 
 
 
