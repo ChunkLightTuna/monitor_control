c16855a44ab5e9b19f3c9b7f68184d837a5544ec
Christopher Renaud Oelerich <chris@oeleri.ch>
2024-06-10 11:07:35 -0500

    ??

--- weather.py
+++ weather.py
@@ -14,7 +14,8 @@ directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW'
 
 
 def wind_dir(degrees: int) -> str:
-    logging.warning(f"{degrees=}")
+    if degrees >= 360:
+        degrees = degrees % 360
     return next(d for i, d in enumerate(directions) if abs(degrees - (i * 22.5)) % (360 - 11.25) < 11.25)
 
 
