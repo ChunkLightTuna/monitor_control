524fd188433ee2bf0e5096aa5dacd1252723a6e7
Christopher Renaud Oelerich <chris@oeleri.ch>
2024-06-14 11:53:22 -0500

    retry logic and logging

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import asyncio
+import logging
+from collections import deque
 from contextlib import asynccontextmanager
+from time import monotonic
 
 from fastapi import FastAPI
 
@@ -11,13 +14,50 @@ from pad import Keypad
 keypad = Keypad()
 menu = MainMenu(keypad)
 
+MAX_INTERVAL = 30
+RETRY_HISTORY = 3
+
+
+# That is, stop after the 3rd failure in a 30 second moving window
+
+def supervise(func, name=None, retry_history=RETRY_HISTORY, max_interval=MAX_INTERVAL):
+    """Simple wrapper function that automatically tries to name tasks"""
+    if name is None:
+        if hasattr(func, '__name__'):  # raw func
+            name = func.__name__
+        elif hasattr(func, 'func'):  # partial
+            name = func.func.__name__
+    return asyncio.create_task(supervisor(func, retry_history, max_interval), name=name)
+
+
+async def supervisor(func, retry_history=RETRY_HISTORY, max_interval=MAX_INTERVAL):
+    """Takes a noargs function that creates a coroutine, and repeatedly tries
+        to run it. It stops is if it thinks the coroutine is failing too often or
+        too fast.
+    """
+    start_times = deque([float('-inf')], maxlen=retry_history)
+    while True:
+        start_times.append(monotonic())
+        try:
+            return await func()
+        except asyncio.CancelledError:
+            logging.exception(f'{func.__name__} cancelled.')
+        except Exception as e:
+            if min(start_times) > monotonic() - max_interval:
+                raise e
+            else:
+                logging.exception(
+                    f'{func.__name__} failed, will retry. Failed because:\n{str(e)}',
+                    stack_info=True
+                )
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     menu.msg_ephemeral(Msg('Monitor', 'Control', align_two=Align.RIGHT))
     tasks = [
-        asyncio.create_task(keypad.run(), name='Keypad'),
-        asyncio.create_task(menu.run(), name='Menu')
+        supervise(keypad.run, name='Keypad'),
+        supervise(menu.run, name='Menu')
     ]
 
     yield
